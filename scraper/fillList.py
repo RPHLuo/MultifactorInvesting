470541d97161f1bf99c2ccefa6461d9211c24600
RPHLuo <RPHLuo@github.com>
2019-05-25 20:20:31 -0400

    update

--- scraper/fillList.py
+++ scraper/fillList.py
@@ -1,6 +1,6 @@
 import pymongo
 def fillList():
-    mongoClient = pymongo.MongoClient("mongodb://mongo:27017/")
+    mongoClient = pymongo.MongoClient("mongodb://localhost:27017/")
     tsx60data = mongoClient['tsx60data']
     constituents = tsx60data['constituents']
     list = open('tsxlist','r')
