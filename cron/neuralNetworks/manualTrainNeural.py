ef54618ad675e28a5aac5e37bb564ef2644a06a7
RPHLuo <Robinpluo@gmail.com>
2019-06-27 23:27:27 -0400

    update run files

--- cron/neuralNetworks/manualTrainNeural.py
+++ cron/neuralNetworks/manualTrainNeural.py
@@ -14,5 +14,5 @@ for ticker in tickers:
     seq_fundamental_cnn.train(ticker=ticker, time_step=200, start=0, epochs=2, path=path)
     seq_technical_cnn.train(ticker=ticker, time_step=200, start=0, epochs=2, path=path)
     target_fundamental_ffnn.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
-    #target_technical_cnn.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
+    target_technical_cnn.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
     target_fundamental_lstm.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
--- cron/updateNeural.py
+++ cron/updateNeural.py
@@ -2,12 +2,17 @@ import os, sys
 path = './neuralNetworks/'
 lib_path = os.path.abspath(os.path.join(path))
 sys.path.append(lib_path)
-import seq_fundamental_lstm, target_fundamental_ffnn, target_technical_cnn
+import seq_fundamental_lstm, seq_technical_lstm, seq_fundamental_cnn, seq_technical_cnn
+import target_fundamental_ffnn, target_technical_cnn, target_fundamental_lstm
 
 supportedStocks = open('./supportedList','r')
 tickers = supportedStocks.read().split('\n')
 tickers = tickers[0:-1]
 for ticker in tickers:
-    target_fundamental_ffnn.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
     seq_fundamental_lstm.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
+    seq_technical_lstm.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
+    seq_fundamental_cnn.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
+    seq_technical_cnn.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
+    target_fundamental_ffnn.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
     target_technical_cnn.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
+    target_fundamental_lstm.train(ticker=ticker, time_step=200, start=0, epochs=10, path=path)
